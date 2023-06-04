83072ab29254f5da61c2c99206a3f8389977f008
Артур Алтынчурин <alturik9@yandex.ru>
2023-06-04 15:16:39 +0500

    calc1

--- HW7/CalculatorFactory.java
+++ HW7/CalculatorFactory.java
@@ -1,22 +1,8 @@
 package HW7;
 
-import src.calculator.CalculatorFactorable;
-import src.calculator.ComplexCalculable;
-import src.calculator.ComplexNumberCalculator;
-import src.loggers.Loggable;
-
-public class LogCalculatorFactory implements CalculatorFactorable {
-
-    private CalculatorFactorable primaryFactory;
-    private Loggable logger;
-
-    public LogCalculatorFactory(CalculatorFactorable primaryFactory, Loggable logger){
-        this.primaryFactory = primaryFactory;
-        this.logger = logger;
-    }
-
+public class CalculatorFactory implements ICalculatorFactorable {
     @Override
-    public ComplexCalculable create(ComplexNumberCalculator primaryArg) {
-        return new LogComplexCalculator(primaryFactory.create(primaryArg), logger);
+    public ComplexCalculable create(ComplexNumberCalculator primaryArg){
+        return new ComplexCalculator(primaryArg);
     }
 }
